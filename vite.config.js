26da6c98dcc253ddee63d09893440be777930d59
Gagan kumar <87gagankumar@gmail.com>
2023-09-14 18:15:07 +0530

    url change

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ 
+  base: '/vue3-learn/', // Replace 'your-repo-name' with your GitHub repository name
+
   plugins: [
     vue(),
   ],
+  
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./prod', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+    
   },
   build: {
     outDir: "prod",
